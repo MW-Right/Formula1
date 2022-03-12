6fb71084fae37b73d495d0799fbec02e54d97cd5
Michael <michaelctwright@gmail.com>
2022-03-12 15:04:28 +0000

    Started work on calendars

---
+++ src/app/calendar/containers/calendar.container.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: '22-calendar',
+    templateUrl: './calendar.container.html',
+    styleUrls: ['./calendar.container.scss']
+})
+export class CalendarContainerComponent {
+    
+}
